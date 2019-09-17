1fb8254c6017a2969528dd16900185cc54f9ec63
praveenkr12 <praveen.kr12@gmail.com>
2019-09-17 14:20:01 -0700

    Update linkextractor.py

--- linkextractor.py
+++ linkextractor.py
@@ -4,5 +4,4 @@ import requests
 from bs4 import BeautifulSoup
 res = requests.get(sys.argv[-1])
 soup = BeautifulSoup(res.text, "html.parser")
-for link in soup.find_all("a"):
-print(link.get("href"))
+for link in soup.find_all("a"):print(link.get("href"))
